00095442d480068a2fc7c977b12e74f0f46e8e64
MongolianHorde <icecakeinc@gmail.com>
2021-08-04 21:11:14 +0530

    Created a simple to do list react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useRef, useEffect } from 'react';
+import TodoList from './Components/TodoList';
+import uuidv4 from 'uuid/v4';
+import M from "materialize-css";
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  useEffect(() => {
+    
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos)  setTodos(storedTodos)
+  }, [])
+
+  useEffect(() =>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  function handleAddTodo(e){
+    const name = todoNameRef.current.value
+    if (name==='') return
+    setTodos(prevTodos =>{
+      return [{id:uuidv4(), name: name,complete: false},...prevTodos ]
+    })  
+    todoNameRef.current.value = null
+  }
+  function handleClearTodos(){
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
+  function deleteTodo(id) {
+    const newTodos = todos.filter(todo => todo.id !== id)
+    setTodos(newTodos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 >Todo List</h1> 
+      <div class="row">
+        <div class="col s10"> 
+          <input ref={todoNameRef}  type="text" placeholder="To Do Item"/>
+        </div>
+        <div class="col s2">
+          <button onClick={handleAddTodo} className="waves-effect waves-light btn">Add</button>
+        </div>      
+      </div>
+      
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+       
+        {todos===null ?
+        <div>
+        <div class="row">
+          <div class="col s10"> 
+            <h5 class="teal-text text-darken-2">{todos.filter(todo => !todo.complete).length} left to do</h5>
+          </div> 
+        </div>
+        <div class="row">
+          <button  onClick={handleClearTodos} className="waves-effect waves-light btn">Clear Complete</button>
+        </div>
+      </div>
+          
+          : null}  
+                          
     </div>
   );
 }
---
+++ src/Components/Todo.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Todo({ todo, toggleTodo, deleteTodo }) {
+    function handleTodoClick(){
+        toggleTodo(todo.id)
+    }
+    function handleDeleteTodo(){
+        deleteTodo(todo.id)
+    }
+    return (
+       <div className="row">
+           <div className="col s10">
+            <label>
+                    <input type="checkbox" checked={todo.complete} onChange={handleTodoClick}/>
+                    <span class="teal-text text-darken-2">{todo.name}</span>
+            </label>
+           </div>
+          
+            
+            <div className="col s2">
+            <button onClick={handleDeleteTodo} className="waves-effect waves-light btn">Delete</button> 
+            </div>
+               
+         
+                
+                       
+        </div>
+    )
+}
+
